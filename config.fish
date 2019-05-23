f03a9bf2339d6a79cdc4cb61a43da810f795b679
reedry <r.yoshida.c@gmail.com>
2019-05-23 22:48:29 +0900

    Add base16-shell to config.fish

--- config.fish
+++ config.fish
@@ -3,3 +3,9 @@ set -x PATH $PATH $PYENV_ROOT/bin
 pyenv init - | source
 
 set -x PATH $PATH $HOME/.cargo/bin
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
