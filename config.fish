618b21c9c52048674f66a206999b3539c069fd64
reedry <r.yoshida.c@gmail.com>
2019-05-27 14:26:50 +0900

    Add env. variables for n command

--- config.fish
+++ config.fish
@@ -4,6 +4,10 @@ pyenv init - | source
 
 set -x PATH $PATH $HOME/.cargo/bin
 
+set -x PREFIX $HOME/.n
+set -x N_PREFIX $HOME/.n
+set -x PATH $PATH $N_PREFIX/bin
+
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
