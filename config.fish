eb69692f3acae2b64ba0b0c0c6840f4c2f164012
reedry <r.yoshida.c@gmail.com>
2023-06-24 13:43:30 +0900

    Fix config.fish

--- config.fish
+++ config.fish
@@ -1,8 +1,9 @@
 set -x LANG ja_JP.UTF-8
 
-set -gx PYENV_ROOT $HOME/.pyenv
-set -x PATH $PATH $PYENV_ROOT/bin
-status --is-interactive; and source (pyenv init -|psub)
+# PYENV
+# set -gx PYENV_ROOT $HOME/.pyenv
+# set -x PATH $PATH $PYENV_ROOT/bin
+# status --is-interactive; and source (pyenv init -|psub)
 
 set -x PATH $PATH $HOME/.cargo/bin
 
@@ -11,12 +12,6 @@ set -x PATH $PATH $GOPATH/bin
 
 set -x PATH $PATH $HOME/.yarn/bin
 
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
-
 set -x PATH $PATH $HOME/.local/bin
 set -x PATH $PATH $HOME/.roswell/bin
 
@@ -24,4 +19,4 @@ set -x PATH $PATH $HOME/.roswell/bin
 source /home/ry/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
 # fish_color
-set fish_color_command blue
+set fish_color_command white
